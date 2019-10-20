24141ffaa02431529ae0c9507785122c09bfafbf
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-20 15:46:52 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -5,7 +5,7 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     create_table :artists do |t| 
       t.string :name 
       t.string :genre 
-      t.string.to_i :age 
+      t.Time.now.to_i :age 
       t.string :hometown 
      end 
   end 
